91111e830914f95b0b2cec5c69adb5e74f57fe7d
Jack-du-ubc <jack.du.ubc@gmail.com>
2023-06-28 21:10:43 +0900

    make future page design available in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,26 @@
 // script.js
 
-function changePage(page) {
-    // Get the content div
-    var contentDiv = document.getElementById('content');
+const navLinks = document.querySelectorAll('.nav-link');
+const pageDivs = document.querySelectorAll('.page');
 
-    // Clear the current content
-    contentDiv.innerHTML = '';
+function showPage(page) {
+    pageDivs.forEach(function (div) {
+        div.style.display = 'none';
+    });
 
-    // Check the page parameter and update the content accordingly
-    if (page === 'home'){
-        contentDiv.innerHTML = '<h1>Welcome to My Portfolio</h1><p>This is the homepage.</p>';
-    } else if (page === 'introduction') {
-        contentDiv.innerHTML = '<h1>Introduction</h1><p>This is the introduction section of my portfolio website.</p>';
-    } else if (page === 'projects') {
-        contentDiv.innerHTML = '<h1>Projects</h1><p>This is the projects section of my portfolio website.</p>';
-    } else if (page === 'contact') {
-        contentDiv.innerHTML = '<h1>Contact</h1><p>This is the contact section of my portfolio website.</p>';
+    const selectedPage = document.getElementById(page);
+    if (selectedPage) {
+        selectedPage.style.display = 'block';
     }
 }
+
+navLinks.forEach(function (link) {
+    link.addEventListener('click', function (event) {
+        event.preventDefault();
+        const page = this.getAttribute('href').substring(1);
+        showPage(page);
+    });
+});
+
+showPage('home');
 
